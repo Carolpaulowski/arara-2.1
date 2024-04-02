f656c31bb9e64ccbade42031112b03584814afd2
Carolpaulowski <160596588+Carolpaulowski@users.noreply.github.com>
2024-04-02 08:01:48 -0300

    js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 for(let i=0; i<botoes.length; i++){
     botoes [i].onclick = function(){
         for(let j=0; j<botoes.length; j++){
-            botoes [j].classList.remove("ativo");
+            botoes[j].classList.remove("ativo");
         }
     }
 }
